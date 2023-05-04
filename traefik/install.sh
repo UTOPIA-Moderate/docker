50a03a5fba78fc69a2637d1cbaf6f851d6e6e416
UTOPIA-Moderate <42368385+UTOPIA-Moderate@users.noreply.github.com>
2023-05-04 14:01:26 +0800

    Update install.sh

--- traefik/install.sh
+++ traefik/install.sh
@@ -3,13 +3,16 @@
 
 #二进制安装traefik
 echo "开始安装二进制"
-curl -o traefik.tar.gz -L https://github.com/traefik/traefik/releases/download/v2.9.8/traefik_v2.9.8_linux_amd64.tar.gz
+curl -o traefik.tar.gz -L https://github.com/traefik/traefik/releases/download/v2.10.1/traefik_v2.10.1_linux_amd64.tar.gz
 tar zxvf traefik.tar.gz -C /usr/local/bin traefik
 rm -f traefik.tar.gz
 traefik version
 
 echo "配置systemctl"
-mkdir -p /etc/traefik/
+mkdir -p /etc/traefik/dynamic
+echo "下载配置文件"
+curl -o /etc/traefik/traefik.yml -L https://raw.githubusercontent.com/UTOPIA-Moderate/docker/main/traefik/traefik.yml
+curl -o /etc/traefik/dynamic/dashboard.yml -L https://raw.githubusercontent.com/UTOPIA-Moderate/docker/main/traefik/dashboard.yml
 
 cat <<EOF > /etc/systemd/system/traefik.service
 [Unit]
