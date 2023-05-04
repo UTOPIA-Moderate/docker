82f48006fccf80ce32b34a108343a144c3075bca
UTOPIA-Moderate <42368385+UTOPIA-Moderate@users.noreply.github.com>
2023-05-04 14:33:35 +0800

    Update install.sh

--- traefik/install.sh
+++ traefik/install.sh
@@ -14,8 +14,8 @@ echo "下载配置文件"
 curl -o /etc/traefik/traefik.yml -L https://raw.githubusercontent.com/UTOPIA-Moderate/docker/main/traefik/traefik.yml
 curl -o /etc/traefik/dynamic/dashboard.yml -L https://raw.githubusercontent.com/UTOPIA-Moderate/docker/main/traefik/dashboard.yml
 
-echo "创建acme.json 并修改权限644"
-mkdir -p /home/traefik && touch /home/traefik/acme.json && chmod 644 /home/traefik/acme.json
+echo "创建acme.json 并修改权限600"
+mkdir -p /home/traefik && touch /home/traefik/acme.json && chmod 600 /home/traefik/acme.json
 
 cat <<EOF > /etc/systemd/system/traefik.service
 [Unit]
